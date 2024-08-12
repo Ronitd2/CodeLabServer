a6911fc186255c1c3a882e3cead146e2b6fbf5ea
Ronitd2 <ronitd552@gmail.com>
2024-08-12 19:38:36 +0530

    compile

--- routehandlers/CompilerHandler.js
+++ routehandlers/CompilerHandler.js
@@ -1,13 +1,36 @@
 const express= require('express');
 const router = express.Router();
-const {python} = require('compile-run');
+const {c, cpp, node, python, java} = require('compile-run');
 router.post('/',async (req,res)=>{
     
     try{
     console.log(req.body);
     const source = `print("Hell0 W0rld!")`;
 
-    let resultPromise = await python.runSource(req.body.sourcecode);
+    if(req.body.program==="C++")
+    {
+        const resultPromise = await cpp.runSource(req.body.sourcecode);    
+
+    }
+    else if(req.body.program==="C")
+    {
+        const resultPromise = await c.runSource(req.body.sourcecode);    
+
+    }
+    else if(req.body.program==="Java")
+    {
+        const resultPromise = await java.runSource(req.body.sourcecode);    
+    }
+    else if(req.body.program==="Python")
+    {
+        const resultPromise = await python.runSource(req.body.sourcecode);      
+    }
+    else if(req.body.program==="Javascript")
+    {
+        const resultPromise = await node.runSource(req.body.sourcecode);    
+  
+    }
+    //let resultPromise = await python.runSource(req.body.sourcecode);
     // resultPromise
     //     .then(result => {
     //         console.log(result);
